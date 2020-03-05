fd9098f2adef510a2af6b2fa75210c1cd8ea32b7
Cécile Kremer <30626850+cecilekremer@users.noreply.github.com>
2020-03-05 21:09:24 +0100

    Update MCMC_generation_interval.R

--- MCMC_generation_interval.R
+++ MCMC_generation_interval.R
@@ -3,29 +3,77 @@
 ## DATA ##
 ##########
 
-rm(list=ls())
-data <- read.csv("G:/My Drive/UHasselt/PhD/HAV outbreak data/2019nCoV/Tianjin/FINAL VERSION/Tianjin cluster cases (updated Feb 29).csv", header=T)
-data$symptomOnset <- as.Date(data$symptom_onset, "%d/%m/%Y")
+rm(list = ls())
 
-Case <- data$CaseID
-Time <- data$symptomOnset - min(data$symptomOnset, na.rm=T) 
+### DATA UPDATED 27/02/2020
+
+Cluster <- c(rep(1,8), rep(5,23), rep(2,8), rep(3,3), rep(4,5), rep(6,4), rep(7,3))
+Case.ID <- c(8,9,31,33,38,83,90,91,66,68,70,71,80,84,88,48,49,51,54,57,58,60,53,61,62,63,67,73,74,78,81,19,20,21,24,25,27,34,40,30,36,39,42,47,52,56,69,2,13,26,44,50,55,77)
+Case <- c(1:length(Case.ID))
+Time <- c(4,4,3,10,14,8,20,3,9,10,14,12,15,15,27,12,14,15,21,22,21,19,21,17,20,21,20,20,23,20,27,9,5,13,10,4,12,7,10,1,4,9,12,17,18,23,22,1,8,8,11,18,10,21)
+vi <- c(0,0,rep(NA,6), rep(NA,18),24,18,rep(NA,3),0,0,32,0,0,32,rep(NA,8),44,NA,0,0,0,NA,NA,0,52)
+
+data <- data.frame(Cluster,Case,Time,vi)
+
+NCases <- length(Case)
 
 # Which cases are possible?
-NCases  <- nrow(data)
 vi.list <- list()
-for (i in 1:NCases){
-  vi.list[[i]] <- as.numeric(unlist(strsplit(as.character(data$Source[i]),",")))
-}
 
-PossibleInfector <- matrix(nrow=NCases,ncol=max(lengths(strsplit(as.character(data$Source),","))))
+vi.list[[3]] <- c(1,2,4:8)
+vi.list[[4]] <- c(1,2,3,5:8)
+vi.list[[5]] <- c(1:4,6,7,8)
+vi.list[[6]] <- c(1,2)
+vi.list[[7]] <- c(1:6,8)
+vi.list[[8]] <- c(1,2)
+
+vi.list[[9]] = vi.list[[10]] = vi.list[[11]] = vi.list[[12]] = vi.list[[13]] = vi.list[[14]] = vi.list[[15]] <- c(6,8)
+vi.list[[16]] <- c(9, 17:22)
+vi.list[[17]] <- c(9, 16,18:22)
+vi.list[[18]] <- c(9, 16,17,19:22)
+vi.list[[19]] <- c(9, 16:18,20:22)
+vi.list[[20]] <- c(9, 16:19,21,22)
+vi.list[[21]] <- c(9, 16:20,22)
+vi.list[[22]] <- c(9, 16:21)
+vi.list[[23]] <- c(9, 16:22, 24:31)
+vi.list[[24]] <- c(9, 27)
+vi.list[[25]] <- c(9, 16:22, 23,24,26:31)
+vi.list[[26]] <- c(9, 16:22, 23:25,27:31)
+vi.list[[29]] <- c(9, 16:22, 23:28,30,31)
+vi.list[[30]] <- c(9, 16:22,23:29,31)
+vi.list[[31]] <- c(9, 16:22, 23:30)
+
+vi.list[[38]] <- c(32,33)
+vi.list[[39]] <- c(32,33)
+
+vi.list[[40]] <- c(41,42)
+vi.list[[41]] <- c(40,42)
+vi.list[[42]] <- c(40,41)
+
+vi.list[[43]] <- c(44,45)
+vi.list[[44]] <- c(43,45,46)
+vi.list[[45]] <- c(43,44)
+vi.list[[47]] <- c(43:46)
+
+vi.list[[51]] <- c(49,50)
+
+vi.list[[52]] <- c(53,54)
+vi.list[[53]] <- c(0)
+vi.list[[54]] <- c(52)
+
+for(i in 1:NCases){
+  if(is.null(vi.list[[i]])) vi.list[[i]] <- vi[i] 
+}
+ 
+
+PossibleInfector <- matrix(nrow=NCases,ncol=max(lengths(vi.list)))
 
 for(i in 1:NCases){
   PossibleInfector[i,1:lengths(vi.list)[i]] <- vi.list[[i]]
 }
-
-NPossibleInfector <- rowSums(!is.na(PossibleInfector))
-IsNotContributorToLikel <-  c(which(PossibleInfector[,1]==0))
-IsContributorToLikel <- c(which(PossibleInfector[,1]!=0))
+NPossibleInfector    <- rowSums(!is.na(PossibleInfector))
+IsNotContributorToLikel <- c(which(vi==0)) # index cases
+IsContributorToLikel <- Case[!Case%in%IsNotContributorToLikel]
 
 ###############
 # likelihood
